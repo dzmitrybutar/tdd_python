f504f99567fc506146710ebf2de94c2dcd899087
dzmitrybutar <verysweetleaf@gmail.com>
2019-05-01 12:18:19 +0300

    Basic view returns HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.http import HttpRequest
 from django.urls import resolve
 from lists.views import home_page
 
@@ -7,3 +8,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolver_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
