a5cf69453063be9b3d338f70e8bedca42900ceba
dzmitrybutar <verysweetleaf@gmail.com>
2019-05-03 15:58:11 +0300

    Fix FT and forloop

--- functional_tests.py
+++ functional_tests.py
@@ -29,8 +29,13 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
